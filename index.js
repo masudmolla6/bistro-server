70af91acbb3a4f13fe83c7d8fe90e302e7da338b
masudmolla6 <masudmolla2937@gmail.com>
2024-07-09 10:45:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,141 @@
+const express = require("express");   
+const cors = require("cors");
+require("dotenv").config(); 
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ygkpv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userCollection = client.db("bistroDb").collection("users");
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartsCollection = client.db("bistroDb").collection("carts");
+
+
+    // users related api.
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.post("/users", async (req, res) => {
+      const user=req.body;
+
+      // Insert Email if User Doesn't Exists,
+      // We Can Do This Many way(1-Email Unique. 2-Upsert. 3-Simple Checking)
+
+      // We Use Simple Email Checking Way-3
+
+      const query = { email: user?.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User Already Exists in The Database", insertedId: null });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc= {
+        $set: {
+          role:"admin"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+    app.delete("/users/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+
+      res.send(result)
+    })
+
+
+    // Review Related Api.
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    // Menu Related Api.
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    // Carts Collections
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+
+      res.send(result);
+    })
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+
+      res.send(result);
+    })
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", async (req, res) => {
+    res.send("Bistro Server is runing");
+})
+
+app.listen(port, () => {
+    console.log("Bistro Server is running on port", port);
+})
